02bf172c6afa7e8ca864e0380235c6ab6d32d73a
justinwoo <moomoowoo@gmail.com>
2020-01-10 00:56:57 +0200

    update

--- default.nix
+++ default.nix
@@ -8,8 +8,8 @@ pkgs.stdenv.mkDerivation rec {
   name = "nixpkgs-fmt";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/justinwoo/tsbot/releases/download/2020-01-07/tsbot";
-    sha256 = "10lsf569s0f0nhw92cr97y4qrmwg655vx6r16sbzhmibhx7qvqxx";
+    url = "https://github.com/justinwoo/tsbot/releases/download/2020-01-09/tsbot";
+    sha256 = "0d517ifw9c0g2pp68g8v383v7lmhn47bspas13s3ja1ncqgnqnqc";
   };
 
   buildInputs = [ pkgs.makeWrapper ];
